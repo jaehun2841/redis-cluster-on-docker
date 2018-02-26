ae496682b4a800339acddb2e1713f47a72c69220
jaehun2841 <jaehun2841@gmail.com>
2018-02-26 14:16:27 +0900

    Local Clustering done. Replication done.

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,20 @@ WORKDIR /usr/local/bin
 
 ## Copy Redis File
 ## 복사/추가 하는파일의 Container내 경로는 항상 절대경로로 작성하여야 한다.
-ADD /redis-conf/master.conf /usr/local/bin/redis.conf
+RUN rm -rf /usr/local/bin/docker-entrypoint.sh
+ADD redis.conf /usr/local/bin/redis.conf
 RUN touch /usr/local/bin/redis.log
+ADD docker-entrypoint.sh /usr/local/bin
 
 ## change access authority
 RUN chmod 755 /usr/local/bin/redis.conf
 RUN chmod 755 /usr/local/bin/redis.log
+RUN chmod 755 /usr/local/bin/docker-entrypoint.sh
+
 RUN chown redis:redis /usr/local/bin/redis.conf
 RUN chown redis:redis /usr/local/bin/redis.log
+RUN chown redis:redis /usr/local/bin/docker-entrypoint.sh
 
+EXPOSE $CLIENTPORT
 ENTRYPOINT ["./docker-entrypoint.sh"]
 CMD [ "redis-server","/usr/local/bin/redis.conf" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,24 @@
 #!/bin/sh
-set -ex
+set -e
 
 ## from redis-3.2
-#sed -i "s/bind 127.0.0.1/bind $CLIENTHOST/g" /etc/redis.conf
+sed -i "s/bind 127.0.0.1/bind $CLIENTHOST/g" /usr/local/bin/redis.conf
 
 ## redis port inside redis.conf
-#sed -i "s/port 6379/port $CLIENTPORT/g" /etc/redis.conf
-#sed -i "s/# requirepass foobared/requirepass $REQUIREPASS/g" /etc/redis.conf
-#sed -i "s/# masterauth <master-password>/masterauth $REQUIREPASS/g" /etc/redis.conf
+sed -i "s/port 6379/port $CLIENTPORT/g" /usr/local/bin/redis.conf
+sed -i "s/# requirepass foobared/requirepass $REQUIREPASS/g" /usr/local/bin/redis.conf
+sed -i "s/# masterauth <master-password>/masterauth $REQUIREPASS/g" /usr/local/bin/redis.conf
 
 ## slaveof <masterip> <masterport> => slaveof $MASTERHOST $MASTERPORT
-#if [ "$MASTERPORT" != "" ];then
-#	sed -i "s/# slaveof <masterip> <masterport>/slaveof $MASTERHOST $MASTERPORT/g" /etc/redis.conf
-#fi
+if [ "$MASTERPORT" != "" ];then
+	sed -i "s/# slaveof <masterip> <masterport>/slaveof $MASTERHOST $MASTERPORT/g" /usr/local/bin/redis.conf
+fi
 
 ## appendfsync everysec => appendfsync $APPENDFSYNC
-#if [ "$APPENDFSYNC" != "" ];then
-#	sed -i "s/appendonly no/appendonly yes/g" /etc/redis.conf
-#	sed -i "s/appendfsync everysec/appendfsync $APPENDFSYNC/g" /etc/redis.conf
-#fi
-
-
- 
-
+if [ "$APPENDFSYNC" != "" ];then
+	sed -i "s/appendonly no/appendonly yes/g" /usr/local/bin/redis.conf
+	sed -i "s/appendfsync everysec/appendfsync $APPENDFSYNC/g" /usr/local/bin/redis.conf
+fi
 
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
