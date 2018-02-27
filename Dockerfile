6e7bf3df7312d80cdb8fa2b254bbdf9767fee391
root <root@localhost.localdomain>
2018-02-27 17:48:32 +0900

    Redis Cluster Configuration 수정

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM redis:4.0.8-alpine
 
 MAINTAINER Carrey (jaehun.lee@ticketlink.co.kr)
-WORKDIR /usr/local/bin
 
 ## Copy Redis File
 ## 복사/추가 하는파일의 Container내 경로는 항상 절대경로로 작성하여야 한다.
@@ -20,5 +19,5 @@ RUN chown redis:redis /usr/local/bin/redis.log
 RUN chown redis:redis /usr/local/bin/docker-entrypoint.sh
 
 EXPOSE $CLIENTPORT
-ENTRYPOINT ["./docker-entrypoint.sh"]
+ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
 CMD [ "redis-server","/usr/local/bin/redis.conf" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,21 +3,15 @@ set -e
 
 ## from redis-3.2
 sed -i "s/bind 127.0.0.1/bind $CLIENTHOST/g" /usr/local/bin/redis.conf
-
-## redis port inside redis.conf
+#
+### redis port inside redis.conf
 sed -i "s/port 6379/port $CLIENTPORT/g" /usr/local/bin/redis.conf
 sed -i "s/# requirepass foobared/requirepass $REQUIREPASS/g" /usr/local/bin/redis.conf
 sed -i "s/# masterauth <master-password>/masterauth $REQUIREPASS/g" /usr/local/bin/redis.conf
-
-## slaveof <masterip> <masterport> => slaveof $MASTERHOST $MASTERPORT
+#
+### slaveof <masterip> <masterport> => slaveof $MASTERHOST $MASTERPORT
 if [ "$MASTERPORT" != "" ];then
 	sed -i "s/# slaveof <masterip> <masterport>/slaveof $MASTERHOST $MASTERPORT/g" /usr/local/bin/redis.conf
-fi
-
-## appendfsync everysec => appendfsync $APPENDFSYNC
-if [ "$APPENDFSYNC" != "" ];then
-	sed -i "s/appendonly no/appendonly yes/g" /usr/local/bin/redis.conf
-	sed -i "s/appendfsync everysec/appendfsync $APPENDFSYNC/g" /usr/local/bin/redis.conf
 fi
 
 # first arg is `-f` or `--some-option`
